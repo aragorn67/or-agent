4a9c3c03ce66234f84cb76576a697cc5c9660e1f
Captain_Tolaros <apostoliselekidis@gmail.com>
2025-09-22 21:17:35 +0300

    Add files via upload

---
+++ # test_run.py
@@ -0,0 +1,17 @@
+# test_run.py
+from model import solve_transport
+
+params = {
+    "plants": ["seattle", "san-diego"],
+    "markets": ["new-york", "chicago", "topeka"],
+    "capacity": {"seattle": 350, "san-diego": 600},
+    "demand": {"new-york": 325, "chicago": 300, "topeka": 275},
+    "distance": {
+        ("seattle","new-york"):2.5, ("seattle","chicago"):1.7, ("seattle","topeka"):1.8,
+        ("san-diego","new-york"):2.5, ("san-diego","chicago"):1.8, ("san-diego","topeka"):1.4
+    },
+    "freight": 90.0
+}
+
+res = solve_transport(params)
+print(res)
